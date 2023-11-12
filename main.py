bc6f90ee3d390a5795f81dfa74d54378ba3ac08f
Damar Jati <damarcodes@gmail.com>
2023-11-12 19:38:48 +0700

    Update main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import IPython.display as ipd
+
+import os
+import json
+import math
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+
+import commons
+import utils
+from data_utils import TextAudioLoader, TextAudioCollate, TextAudioSpeakerLoader, TextAudioSpeakerCollate
+from models import SynthesizerTrn
+from text.symbols import symbols
+from text import text_to_sequence
+
+from scipy.io.wavfile import write
+
+
+def get_text(text, hps):
+    text_norm = text_to_sequence(text, hps.data.text_cleaners)
+    if hps.data.add_blank:
+        text_norm = commons.intersperse(text_norm, 0)
+    text_norm = torch.LongTensor(text_norm)
+    return text_norm
+
+hps = utils.get_hparams_from_file("/content/drive/MyDrive/vit/configs/config-single-speaker.json")
+
+net_g = SynthesizerTrn(
+    len(symbols),
+    hps.data.filter_length // 2 + 1,
+    hps.train.segment_size // hps.data.hop_length,
+    **hps.model).cuda()
+_ = net_g.eval()
+
+_ = utils.load_checkpoint("/content/drive/MyDrive/vit/model/ameth.pth", net_g, None)
+
+
+stn_tst = get_text("こんにちは。何かお手伝いできることはありますか?", hps)
+with torch.no_grad():
+    x_tst = stn_tst.cuda().unsqueeze(0)
+    x_tst_lengths = torch.LongTensor([stn_tst.size(0)]).cuda()
+    audio = net_g.infer(x_tst, x_tst_lengths, noise_scale=.667, noise_scale_w=1, length_scale=1.2)[0][0,0].data.cpu().float().numpy()
+ipd.display(ipd.Audio(audio, rate=hps.data.sampling_rate, normalize=False))
